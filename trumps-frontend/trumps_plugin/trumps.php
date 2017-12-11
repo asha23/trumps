7a6f263bad1c5066d2b7426f1a50323a6557fa18
Ash Whiting <whiting14@googlemail.com>
2017-12-11 14:32:21 +0000

    added the beginnings of a plugin so we can display this inside a WordPress post

---
+++ trumps-frontend/trumps_plugin/trumps.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+  * @wordpress-plugin
+  * Plugin Name: Trumps
+  * Plugin URI: http://github.com/asha23
+  * Description: Just a simple plugin to hook up the Trumps Application
+  * Version: 0.1
+  * Author: Ash WP_Customize_Header_Image_Control::print_header_image_template
+  * Author URI: http://ashwhiting.com
+*/
+
+// Get scripts and class
+
+// Do the shortcode
+
+function trumps_shortcode() {
+?>
+
+<?php
+}
+add_shortcode('trumps', 'trumps_shortcode');
